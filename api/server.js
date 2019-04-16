e4c7da0ff9f326b69eed72bff19cbc48066a05a0
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-04-16 09:24:24 -0700

    commiting for deploy to Heroku

--- api/server.js
+++ api/server.js
@@ -28,26 +28,26 @@ server.use('/api/register', registerRouter)
 // server.use('/api/students', studentsRouter)
 // server.use('/api/projects', projectsRouter)
 
-server.get('/users', async (req, res) => {
-  const users = await db('users');
-  res.status(200).json(users);
-})
-server.get('/api/projects', async (req, res) => {
-  const projects = await db('projects');
-  res.status(200).json(projects);
-})
-server.get('/api/students', async (req, res) => {
-  const students = await db('students');
-  res.status(200).json(students);
-})
-server.get('/api/messages', async (req, res) => {
-  const messages = await db('messages');
-  res.status(200).json(messages);
-})
-server.get('/api/student-projects', async (req, res) => {
-  const studentsProjects = await db('student_project');
-  res.status(200).json(studentsProjects);
-})
+// server.get('/users', async (req, res) => {
+//   const users = await db('users');
+//   res.status(200).json(users);
+// })
+// server.get('/api/projects', async (req, res) => {
+//   const projects = await db('projects');
+//   res.status(200).json(projects);
+// })
+// server.get('/api/students', async (req, res) => {
+//   const students = await db('students');
+//   res.status(200).json(students);
+// })
+// server.get('/api/messages', async (req, res) => {
+//   const messages = await db('messages');
+//   res.status(200).json(messages);
+// })
+// server.get('/api/student-projects', async (req, res) => {
+//   const studentsProjects = await db('student_project');
+//   res.status(200).json(studentsProjects);
+// })
 
 
 
