69c695f579ec003cff1d21f44b37a32c679c902c
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-04-16 10:24:08 -0700

    more bug testing

--- routes/register-route.js
+++ routes/register-route.js
@@ -10,9 +10,13 @@ router.post('/', async (req, res) => {
     if (!user.username || !user.password) {
       res.status(400).json({ message: "please fill in all fields" });
     } else {
+      console.log("else");
       user.password = bcrypt.hashSync(user.password, 12);
+      console.log("bcrypt");
       const [id] = await db('users').insert(user);
+      console.log("id: ", id);
       const token = await generateToken(user);
+      console.log("token: ", token);
       res.status(201).json({ token });
     }
   } catch (error) {
