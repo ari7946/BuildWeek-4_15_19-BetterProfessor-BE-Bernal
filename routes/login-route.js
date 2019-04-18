d9a913699158e40d9a3d7cb78b8f9c8c6a27a6a2
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-04-18 01:25:20 -0700

    added debugging statements

--- routes/login-route.js
+++ routes/login-route.js
@@ -5,16 +5,23 @@ const secrets = require('../api/secrets.js');
 const db = require('../data/dbConfig.js');
 
 router.post('/', async (req, res) => {
+  console.log('Login Route')
   try {
     let { username, password } = req.body;
+    console.log('Username: ', username);
+    console.log('password: ', password);
     const user = await db('users').where({ username }).first();
+    console.log('user: ', user);
     if (user && bcrypt.compareSync(password, user.password)) {
       const token = generateToken(user);
+      console.log('token: ', token);
       res.status(200).json({ token })
     } else {
+      console.log('error 401');
       res.status(401).json({ message: "invalid credentials" });
     }
   } catch (error) {
+    console.log('error 500');
     res.status(500).json({ message: "internal server error" });
   }
 });
