1edff7604d5e1f42e7c10ad90c657cc381e02ce0
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-04-19 07:01:08 -0700

    added tests for projects and students

--- routes/projects-route.spec.js
+++ routes/projects-route.spec.js
@@ -4,34 +4,27 @@ const projects = require('./login-route.js');
 const db = require('../data/dbConfig.js');
 const bcrypt = require('bcryptjs');
 
-let token;
-const pass = bcrypt.hashSync('pass', 12);
-beforeAll(async (done) => {
-  await db('users').truncate();
-  request(server).post('/api/login').send({ username: "test", password: pass }).end((err, response) => {
-    token = response.body.token; // save the token
-    done();
-  })
-})
+
 
 describe('Projects Route', () => {
   describe('GET /api/projects', () => {
 
-    it('Route Restriction should return status code ', async () => {
+    it('Route Restriction should return status code 401', async () => {
       return request(server).get('/api/projects').then(res => {
         expect(res.statusCode).toBe(401);
       })
     })
 
-    it('Route Restriction should return status code ', async () => {
+    it('Route Restriction should return status code 200 and JSON data', async () => {
+      await db('users').truncate();
+      const pass = bcrypt.hashSync('pass', 12);
+      await db('users').insert({ username: "test", password: pass })
+      const res = await request(server).post('/api/login').send({ username: "test", password: "pass" });
+      const token = res.body.token;
       return request(server).get('/api/projects').set('Authorization', `${token}`).then(res => {
         expect(res.status).toBe(200);
-      })
-    })
-
-    it('should return JSON', () => {
-      return request(server).get('/').then(res => {
         expect(res.type).toBe('application/json')
+        console.log(res.body);
       })
     })
 
--- routes/students-route.spec.js
+++ routes/students-route.spec.js
@@ -4,36 +4,23 @@ const students = require('./login-route.js');
 const db = require('../data/dbConfig.js');
 const bcrypt = require('bcryptjs');
 
-// testing endpoints
-//returns correct http status code
-
-// beforeEach(async () => {
-//   await db('users').truncate();
-//   await db('students').truncate();
-//   await db('projects').truncate();
-//   await db('messages').truncate();
-//   await db('student_project').truncate();
-
-//   const pass = bcrypt.hashSync('pass', 12);
-//   const users = await db('users').insert({ username: "test", password: pass })
-//   const students = await db('students').insert({ firstname: "lambda", lastname: "school", email: "lambda@gmail.com", })
-//   const projects = await db('projects').insert({ "project_name": "Static Code Checker", "project_deadline": "2019-05-08 12:00:00", "feedback_deadline": "2019-05-10 12:00:00", "recommendation_deadline": "2019-06-01 12:00:00" });
-//   await db('messages').insert({ message: "Message1" })
-//   const messages = await db('messages').insert({ message: "Message2" })
-//   const studentProject = await db('student_project').insert({ student_id: 1, project_id: 1, professor_id: 1, student_message: 1, professor_message: 2 })
-// })
-
 describe('Students Route', () => {
   describe('GET /api/students', () => {
 
-    it('Should return status code 200', async () => {
+    it('Route Restriction should return status code 401', async () => {
       return request(server).get('/api/students').then(res => {
-        expect(res.status).toBe(200)
+        expect(res.statusCode).toBe(401);
       })
     })
 
-    it('should return JSON', () => {
-      return request(server).get('/api/students').then(res => {
+    it('Route Restriction should return status code 200 and JSON data', async () => {
+      await db('users').truncate();
+      const pass = bcrypt.hashSync('pass', 12);
+      await db('users').insert({ username: "test", password: pass })
+      const res = await request(server).post('/api/login').send({ username: "test", password: "pass" });
+      const token = res.body.token;
+      return request(server).get('/api/students').set('Authorization', `${token}`).then(res => {
+        expect(res.status).toBe(200);
         expect(res.type).toBe('application/json')
       })
     })
